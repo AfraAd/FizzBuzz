feefe04c7a218134381bf801d5cc09eaf45a751a
Afra-A-A <afraazad25@gmail.com>
2023-09-12 20:41:41 -0400

    refactored to extract helper method

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int n = 1000;
+        int outputNum;
+
+        if (n % 5 == 0 && n % 3 == 0){
+            outputNum = (n-1)/5 + (n-1)/3;
+        } else if (n % 5 == 0) {
+            outputNum = (n-1)/5 + n/3;
+        } else if (n % 3 == 0) {
+            outputNum = n/5 + (n-1)/3;
+        }else {
+            outputNum = n/5 + n/3;
+        }
+
+        System.out.println(outputNum);
+    }
 }
